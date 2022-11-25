f0c4e6f18d79b59153d53debc5e69ded73efd7ed
KennethEspinoza14 <116386154+KennethEspinoza14@users.noreply.github.com>
2022-11-25 15:49:41 -0600

    otro avance

---
+++ Lab7P2_Kennethespinoza/src/lab7p2_kennethespinoza/jugador.java
@@ -0,0 +1,142 @@
+package lab7p2_kennethespinoza;
+
+/**
+ *
+ * @author Kenneth
+ */
+public class jugador {
+
+    private String nombre;
+    private int dorsal;
+    private int goles;
+    private int asistencias;
+    private int partidos_jugados;
+    private int tarjetas_amarillas;
+    private int tarjetas_rojas;
+    private int faltas_cometidas;
+    private int faltas_recibidas;
+    private String posición;
+    private int porterias_imbatidas;
+
+    public jugador(String nombre, int dorsal, int goles, int asistencias, int partidos_jugados, int tarjetas_amarillas, int tarjetas_rojas, int faltas_cometidas, int faltas_recibidas, String posición) {
+        this.nombre = nombre;
+        this.dorsal = dorsal;
+        this.goles = goles;
+        this.asistencias = asistencias;
+        this.partidos_jugados = partidos_jugados;
+        this.tarjetas_amarillas = tarjetas_amarillas;
+        this.tarjetas_rojas = tarjetas_rojas;
+        this.faltas_cometidas = faltas_cometidas;
+        this.faltas_recibidas = faltas_recibidas;
+        this.posición = posición;
+    }
+
+    public jugador(String nombre, int dorsal, int goles, int asistencias, int partidos_jugados, int tarjetas_amarillas, int tarjetas_rojas, int faltas_cometidas, int faltas_recibidas, String posición, int porterias_imbatidas) {
+        this.nombre = nombre;
+        this.dorsal = dorsal;
+        this.goles = goles;
+        this.asistencias = asistencias;
+        this.partidos_jugados = partidos_jugados;
+        this.tarjetas_amarillas = tarjetas_amarillas;
+        this.tarjetas_rojas = tarjetas_rojas;
+        this.faltas_cometidas = faltas_cometidas;
+        this.faltas_recibidas = faltas_recibidas;
+        this.posición = posición;
+        this.porterias_imbatidas = porterias_imbatidas;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getDorsal() {
+        return dorsal;
+    }
+
+    public void setDorsal(int dorsal) {
+        this.dorsal = dorsal;
+    }
+
+    public int getGoles() {
+        return goles;
+    }
+
+    public void setGoles(int goles) {
+        this.goles = goles;
+    }
+
+    public int getAsistencias() {
+        return asistencias;
+    }
+
+    public void setAsistencias(int asistencias) {
+        this.asistencias = asistencias;
+    }
+
+    public int getPartidos_jugados() {
+        return partidos_jugados;
+    }
+
+    public void setPartidos_jugados(int partidos_jugados) {
+        this.partidos_jugados = partidos_jugados;
+    }
+
+    public int getTarjetas_amarillas() {
+        return tarjetas_amarillas;
+    }
+
+    public void setTarjetas_amarillas(int tarjetas_amarillas) {
+        this.tarjetas_amarillas = tarjetas_amarillas;
+    }
+
+    public int getTarjetas_rojas() {
+        return tarjetas_rojas;
+    }
+
+    public void setTarjetas_rojas(int tarjetas_rojas) {
+        this.tarjetas_rojas = tarjetas_rojas;
+    }
+
+    public int getFaltas_cometidas() {
+        return faltas_cometidas;
+    }
+
+    public void setFaltas_cometidas(int faltas_cometidas) {
+        this.faltas_cometidas = faltas_cometidas;
+    }
+
+    public int getFaltas_recibidas() {
+        return faltas_recibidas;
+    }
+
+    public void setFaltas_recibidas(int faltas_recibidas) {
+        this.faltas_recibidas = faltas_recibidas;
+    }
+
+    public String getPosición() {
+        return posición;
+    }
+
+    public void setPosición(String posición) {
+        this.posición = posición;
+    }
+
+    public int getPorterias_imbatidas() {
+        return porterias_imbatidas;
+    }
+
+    public void setPorterias_imbatidas(int porterias_imbatidas) {
+        this.porterias_imbatidas = porterias_imbatidas;
+    }
+
+    @Override
+    public String toString() {
+        return "jugador{" + "nombre=" + nombre + ", dorsal=" + dorsal + ", goles=" + goles + ", asistencias=" + asistencias + ", partidos_jugados=" + partidos_jugados + ", tarjetas_amarillas=" + tarjetas_amarillas + ", tarjetas_rojas=" + tarjetas_rojas + ", faltas_cometidas=" + faltas_cometidas + ", faltas_recibidas=" + faltas_recibidas + ", posici\u00f3n=" + posición + ", porterias_imbatidas=" + porterias_imbatidas + '}';
+    }
+
+    
+}
--- Lab7P2_Kennethespinoza/src/lab7p2_kennethespinoza/mundial.java
+++ Lab7P2_Kennethespinoza/src/lab7p2_kennethespinoza/mundial.java
@@ -42,10 +42,15 @@ public class mundial extends javax.swing.JFrame {
         jt_nombre_reg = new javax.swing.JTextField();
         pf_pass_reg = new javax.swing.JPasswordField();
         jButton2 = new javax.swing.JButton();
-        jTabbedPane1 = new javax.swing.JTabbedPane();
-        jTabbedPane2 = new javax.swing.JTabbedPane();
-        jTabbedPane3 = new javax.swing.JTabbedPane();
-        jTabbedPane4 = new javax.swing.JTabbedPane();
+        jd_verEncuentros = new javax.swing.JDialog();
+        ver_encuentros = new javax.swing.JTabbedPane();
+        jPanel2 = new javax.swing.JPanel();
+        jPanel11 = new javax.swing.JPanel();
+        jPanel12 = new javax.swing.JPanel();
+        jTabbedPane10 = new javax.swing.JTabbedPane();
+        jPanel10 = new javax.swing.JPanel();
+        jPanel4 = new javax.swing.JPanel();
+        jPanel1 = new javax.swing.JPanel();
 
         jLabel1.setFont(new java.awt.Font("Segoe UI", 0, 24)); // NOI18N
         jLabel1.setText("Bienvenido ");
@@ -179,27 +184,126 @@ public class mundial extends javax.swing.JFrame {
                 .addContainerGap(78, Short.MAX_VALUE))
         );
 
+        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
+        jPanel2.setLayout(jPanel2Layout);
+        jPanel2Layout.setHorizontalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 388, Short.MAX_VALUE)
+        );
+        jPanel2Layout.setVerticalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 255, Short.MAX_VALUE)
+        );
+
+        ver_encuentros.addTab("tab1", jPanel2);
+
+        javax.swing.GroupLayout jPanel11Layout = new javax.swing.GroupLayout(jPanel11);
+        jPanel11.setLayout(jPanel11Layout);
+        jPanel11Layout.setHorizontalGroup(
+            jPanel11Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 388, Short.MAX_VALUE)
+        );
+        jPanel11Layout.setVerticalGroup(
+            jPanel11Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 255, Short.MAX_VALUE)
+        );
+
+        ver_encuentros.addTab("tab2", jPanel11);
+
+        javax.swing.GroupLayout jPanel12Layout = new javax.swing.GroupLayout(jPanel12);
+        jPanel12.setLayout(jPanel12Layout);
+        jPanel12Layout.setHorizontalGroup(
+            jPanel12Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 388, Short.MAX_VALUE)
+        );
+        jPanel12Layout.setVerticalGroup(
+            jPanel12Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 255, Short.MAX_VALUE)
+        );
+
+        ver_encuentros.addTab("tab3", jPanel12);
+
+        javax.swing.GroupLayout jd_verEncuentrosLayout = new javax.swing.GroupLayout(jd_verEncuentros.getContentPane());
+        jd_verEncuentros.getContentPane().setLayout(jd_verEncuentrosLayout);
+        jd_verEncuentrosLayout.setHorizontalGroup(
+            jd_verEncuentrosLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(jd_verEncuentrosLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(jd_verEncuentrosLayout.createSequentialGroup()
+                    .addContainerGap()
+                    .addComponent(ver_encuentros, javax.swing.GroupLayout.DEFAULT_SIZE, 388, Short.MAX_VALUE)
+                    .addContainerGap()))
+        );
+        jd_verEncuentrosLayout.setVerticalGroup(
+            jd_verEncuentrosLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(jd_verEncuentrosLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(jd_verEncuentrosLayout.createSequentialGroup()
+                    .addContainerGap()
+                    .addComponent(ver_encuentros, javax.swing.GroupLayout.DEFAULT_SIZE, 288, Short.MAX_VALUE)
+                    .addContainerGap()))
+        );
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jTabbedPane1.addTab("tab1", jTabbedPane2);
-        jTabbedPane1.addTab("tab2", jTabbedPane3);
-        jTabbedPane1.addTab("tab3", jTabbedPane4);
+        javax.swing.GroupLayout jPanel10Layout = new javax.swing.GroupLayout(jPanel10);
+        jPanel10.setLayout(jPanel10Layout);
+        jPanel10Layout.setHorizontalGroup(
+            jPanel10Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 388, Short.MAX_VALUE)
+        );
+        jPanel10Layout.setVerticalGroup(
+            jPanel10Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 255, Short.MAX_VALUE)
+        );
+
+        jTabbedPane10.addTab("Agregar selección", jPanel10);
+
+        javax.swing.GroupLayout jPanel4Layout = new javax.swing.GroupLayout(jPanel4);
+        jPanel4.setLayout(jPanel4Layout);
+        jPanel4Layout.setHorizontalGroup(
+            jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 388, Short.MAX_VALUE)
+        );
+        jPanel4Layout.setVerticalGroup(
+            jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 255, Short.MAX_VALUE)
+        );
+
+        jTabbedPane10.addTab("Agregar Jugador", jPanel4);
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 388, Short.MAX_VALUE)
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 255, Short.MAX_VALUE)
+        );
+
+        jTabbedPane10.addTab("Crear/Editar Partido", jPanel1);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGap(17, 17, 17)
-                .addComponent(jTabbedPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 699, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(39, Short.MAX_VALUE))
+            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(layout.createSequentialGroup()
+                    .addContainerGap()
+                    .addComponent(jTabbedPane10)
+                    .addContainerGap()))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jTabbedPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 395, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(35, Short.MAX_VALUE))
+            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(layout.createSequentialGroup()
+                    .addContainerGap()
+                    .addComponent(jTabbedPane10)
+                    .addContainerGap()))
         );
 
         pack();
@@ -249,15 +353,26 @@ public class mundial extends javax.swing.JFrame {
         jd_registro.setModal(true);
         jd_registro.pack();
         jd_registro.setLocationRelativeTo(this);
-
         jd_registro.setVisible(true);
     }
+      
+      private void aparecer_encuentros(){
+          
+          jd_verEncuentros.setModal(true);
+          jd_verEncuentros.pack();
+        jd_verEncuentros.setLocationRelativeTo(this);
+        jd_verEncuentros.setVisible(true);
+        
+
+        }
+      
+      public void aparecer_menu(){
+          this.setVisible(false);
+      }
      
      public void entrar() {
-         
         boolean login = false;
 
-         
         if (jt_user.getText().equals(a.getNombre()) && pf_pass.getText().equals(a.getPass())) {
             JOptionPane.showMessageDialog(this, "Bienvenido al sistema administrador");
             login = true;
@@ -269,8 +384,10 @@ public class mundial extends javax.swing.JFrame {
             if (jt_user.getText().equals(in.getListausuarios().get(i).getNombre()) && pf_pass.getText().equals(in.getListausuarios().get(i).getPass())) {
                 JOptionPane.showMessageDialog(this, "Bienvenido al sistema");
                 login = true;
-                this.setVisible(true);
                 jd_login.setVisible(false);
+                this.setVisible(false);
+                aparecer_encuentros();
+
             }
 
         }
@@ -325,17 +442,34 @@ public class mundial extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel5;
     private javax.swing.JLabel jLabel6;
     private javax.swing.JLabel jLabel7;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel10;
+    private javax.swing.JPanel jPanel11;
+    private javax.swing.JPanel jPanel12;
+    private javax.swing.JPanel jPanel2;
+    private javax.swing.JPanel jPanel3;
+    private javax.swing.JPanel jPanel4;
+    private javax.swing.JPanel jPanel5;
+    private javax.swing.JPanel jPanel6;
+    private javax.swing.JPanel jPanel7;
+    private javax.swing.JPanel jPanel8;
+    private javax.swing.JPanel jPanel9;
     private javax.swing.JTabbedPane jTabbedPane1;
+    private javax.swing.JTabbedPane jTabbedPane10;
     private javax.swing.JTabbedPane jTabbedPane2;
-    private javax.swing.JTabbedPane jTabbedPane3;
-    private javax.swing.JTabbedPane jTabbedPane4;
+    private javax.swing.JTabbedPane jTabbedPane6;
+    private javax.swing.JTabbedPane jTabbedPane7;
+    private javax.swing.JTabbedPane jTabbedPane8;
+    private javax.swing.JTabbedPane jTabbedPane9;
     private javax.swing.JButton jb_registro;
     private javax.swing.JDialog jd_login;
     private javax.swing.JDialog jd_registro;
+    private javax.swing.JDialog jd_verEncuentros;
     private javax.swing.JTextField jt_nombre_reg;
     private javax.swing.JTextField jt_user;
     private javax.swing.JPasswordField pf_pass;
     private javax.swing.JPasswordField pf_pass_reg;
+    private javax.swing.JTabbedPane ver_encuentros;
     // End of variables declaration//GEN-END:variables
 
      static archivos_usuarios in
